81827a5aba976085a6341357830077a6d52db7e0
AzatResh <Azat.R.2003@mail.ru>
2023-07-19 20:44:43 +0600

    - custom text add textalign

--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -6,19 +6,23 @@ class ModifiedText extends StatelessWidget{
   final Color color;
   final double size;
   final FontWeight fontWeight;
+  final TextAlign textAlign;
+  final TextOverflow overflow;
 
   const ModifiedText({
     super.key, 
     required this.text, 
     this.color = Colors.white, 
     required this.size, 
-    this.fontWeight = FontWeight.normal
+    this.fontWeight = FontWeight.normal,
+    this.textAlign = TextAlign.center,
+    this.overflow = TextOverflow.clip,
     });
 
   @override
   Widget build(BuildContext context) {
     return(
-      Text(text, style: GoogleFonts.roboto(color: color, fontSize: size, fontWeight: fontWeight),)
+      Text(text, style: GoogleFonts.roboto(color: color, fontSize: size, fontWeight: fontWeight,), textAlign: textAlign, overflow: overflow,)
     );
   }
 }
